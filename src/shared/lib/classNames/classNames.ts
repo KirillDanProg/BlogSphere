94aa9b7a5678ef5c9d403971d8ef153619ce071e
KirillDanProg <kirill2898v@gmail.com>
2023-02-05 20:24:13 +0300

    refactor: style refactoring

--- src/shared/lib/classNames/classNames.ts
+++ src/shared/lib/classNames/classNames.ts
@@ -1,9 +1,9 @@
 type Mode = Record<string, string | boolean>
 
-export const classNames = (className: string, mode: Mode, additional: string[]) => {
+export const  classNames = (className: string, mode: Mode = {}, additional: string[] = []) => {
     const classNamesArr = [
         className,
-        ...additional,
+        ...additional.filter(Boolean),
         ...Object.entries(mode)
             .filter(([key, value]) => Boolean(value))
             .map(([key, value]) => key)
