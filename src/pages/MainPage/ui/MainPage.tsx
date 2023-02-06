2bedc78da27d76055200855fd0b4dd047ad5c309
KirillDanProg <kirill2898v@gmail.com>
2023-02-06 19:56:57 +0300

    feat: internationalization added (ru/en)

--- src/pages/MainPage/ui/MainPage.tsx
+++ src/pages/MainPage/ui/MainPage.tsx
@@ -1,10 +1,13 @@
 import React from 'react';
+import {useTranslation} from "react-i18next";
 
 const MainPage = () => {
+    const {t} = useTranslation('main');
+
     return (
-        <div>
-            Main page
-        </div>
+            <div>
+                {t("title")}
+            </div>
     );
 };
 
