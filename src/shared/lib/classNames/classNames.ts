897e4706192061a1a62751e0cbfa358057aebe8c
KirillDanProg <kirill2898v@gmail.com>
2023-02-08 22:32:53 +0300

    feat: jest added/test for classNames.ts added

--- src/shared/lib/classNames/classNames.ts
+++ src/shared/lib/classNames/classNames.ts
@@ -1,12 +1,15 @@
 type Mode = Record<string, string | boolean>
 
 export const classNames = (className: string, mode: Mode = {}, additional: string[] = []) => {
-    const classNamesArr = [
-        className,
-        ...additional.filter(Boolean),
-        ...Object.entries(mode)
-            .filter(([key, value]) => Boolean(value))
-            .map(([key, value]) => key)
-    ]
-    return classNamesArr.join(' ')
-}
+  const modeArr = [
+    ...Object.entries(mode)
+      .filter(([key, value]) => Boolean(value))
+      .map(([key, value]) => key)
+  ];
+  const classNamesArr = [
+    className,
+    ...modeArr,
+    ...additional.filter(Boolean)
+  ];
+  return classNamesArr.join(' ');
+};
