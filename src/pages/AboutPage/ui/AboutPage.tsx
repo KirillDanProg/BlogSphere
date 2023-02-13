c7134f3bfd3f34a4ab4ddc224db26e40ce93e1cd
KirillDanProg <kirill2898v@gmail.com>
2023-02-13 20:39:39 +0300

    refactor: eslint updated

--- src/pages/AboutPage/ui/AboutPage.tsx
+++ src/pages/AboutPage/ui/AboutPage.tsx
@@ -1,12 +1,12 @@
 import { useTranslation } from 'react-i18next'
 
 const AboutPage = () => {
-  const { t } = useTranslation('about')
-  return (
+    const { t } = useTranslation('about')
+    return (
         <div>
             {t('title')}
         </div>
-  )
+    )
 }
 
 export default AboutPage
