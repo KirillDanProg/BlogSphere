7eb69b329aa391dd9c7150248489a58dde6606e0
KirillDanProg <kirill2898v@gmail.com>
2023-06-05 08:39:05 +0300

    refactoring

--- src/features/articleRecommendations/api/index.ts
+++ src/features/articleRecommendations/api/index.ts
@@ -1,7 +1,6 @@
 import { rtkApi } from 'shared/rtkApi'
 import { type ArticleType } from 'entities/Arcticle'
 
-// Define a service using a base URL and expected endpoints
 const articleRecommendationsApi = rtkApi.injectEndpoints({
     endpoints: (builder) => ({
         fetchArticleRecommendations: builder.query<ArticleType[], number>({
